9e50c33a053d037ddcace736b14f6a50f3dab42c
GRACE NG XIN YI <20018621@myrp.edu.sg>
2021-05-07 21:16:32 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20018621/emptyactivity_demosimpleclick/MainActivity.java
@@ -0,0 +1,60 @@
+package sg.edu.rp.c346.id20018621.emptyactivity_demosimpleclick;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Step 1: Declare the field variation
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+    ToggleButton tbtn;
+    RadioGroup rgGender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Step 2: Link the field variables to UI components in layout
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add your code for the action
+                String userInput = etInput.getText().toString();
+                if(rgGender.getCheckedRadioButtonId() == R.id.radioButtonGenderMale){
+                    userInput = "He says " + userInput;
+                } else{
+                    userInput = "She says " + userInput;
+                }
+                tvDisplay.setText(userInput);
+            }
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add your code for the action
+                if(!tbtn.isChecked()){
+                    etInput.setEnabled(true);
+                } else{
+                    etInput.setEnabled(false);
+                }
+            }
+        });
+    }
+}
+
